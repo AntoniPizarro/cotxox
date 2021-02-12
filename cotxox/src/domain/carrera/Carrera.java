b8e36b7b1d8e6c4fdf986ddf9df6614341f0ad87
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-02-12 14:50:52 +0100

    Proyecto

---
+++ cotxox/src/domain/carrera/Carrera.java
@@ -0,0 +1,69 @@
+package domain.carrera;
+
+public class Carrera {
+
+    String targetaCredito = "";
+    String origen = "";
+    String destino = "";
+    double distancia = 0d;
+    int tiempoEsperado = 0;
+    int tiempoCarrera = 0;
+    double costeTotal = 0;
+    int propina = 0;
+
+    Carrera(String targeta){
+
+    }
+    public String getTargetaCredito(){
+
+    }
+    public void setOrigen(String nuevoOrigen){
+
+    }
+    public String getOrigen(){
+
+    }
+    public void setDestino(String nuevoDestino){
+
+    }
+    public String getDestino(){
+
+    }
+    public void setDistancia(double nuevaDistancia){
+
+    }
+    public double getDistancia(){
+
+    }
+    public double getCosteEsperado(){
+
+    }
+    public int getTiempoEsperado(){
+
+    }
+    public void setTiempoEsperado(int nuevoTiempoEsperado){
+
+    }
+    public int getTiempoCarrera(){
+
+    }
+    public void setTiempoCarrera(int nuevoTiempoCarrera){
+
+    }
+    public void setConductor(Conductor nuevoConductor){
+
+    }
+    public Conductor getConductor(){
+
+    }
+    public void asignarConductor(PoolConductores conductores){
+
+    }
+    public void realizarPago(double importe){
+
+    }
+    public double getCosteTotal(){
+        
+    }
+
+}
---
+++ cotxox/src/domain/tarifa/Tarifa.java
@@ -0,0 +1,23 @@
+package domain.tarifa;
+
+import org.foobarspam.cotxox.carrera.Carrera;
+
+public class Tarifa {
+
+    final static double COSTE_MILLA = 0;
+    final static double COSTE_MINUTO = 0;
+    final static double COSTE_MINIMO = 0;
+    final static double PORCENTAJE_COMISION = 0;
+
+    Tarifa();
+
+    public static double getCosteDistancia(double distancia){
+        
+    }
+    public static double getCosteTiempo(int tiempo){
+        
+    }
+    public static double getCosteTotalEsperado(Carrera carrera){
+        
+    }
+}
